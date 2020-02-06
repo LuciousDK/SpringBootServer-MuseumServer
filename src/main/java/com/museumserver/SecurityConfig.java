73b9135e095936ea6ac672b4bf7568fafde9ec40
Luat Dinh Bui <ldinhbui@gmail>
2020-02-06 08:23:12 +0000

    Assigned Authentication Requirements for Specific Requests.

--- src/main/java/com/museumserver/SecurityConfig.java
+++ src/main/java/com/museumserver/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.museumserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,10 +37,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		http
 			.authorizeRequests()
-			.anyRequest()
-			.authenticated()
+			.antMatchers(HttpMethod.GET, "/exhibitions","/artworks","/media","/beacons","/exhibition/**","/artwork/**","/media/**","/beacon/**").permitAll()
+			.antMatchers(HttpMethod.PUT,"/exhibition/**","/artwork/**","/media/**","/beacon/**").authenticated()
+			.antMatchers(HttpMethod.POST,"/exhibition/**","/artwork/**","/media/**","/beacon/**").authenticated()
+			.antMatchers(HttpMethod.DELETE,"/exhibition/**","/artwork/**","/media/**","/beacon/**").authenticated()
+			.antMatchers(HttpMethod.GET,"/administrators/**","/administrator/**").authenticated()
 			.and()
-			.httpBasic();
+			.httpBasic()
+			.and()
+			.logout();
 	}
 	
 }
